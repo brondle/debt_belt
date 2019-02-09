149608211f579de2fcdb61070be6c3b5e9ca9279
Brent Bailey <bb2906@nyu.edu>
2019-02-09 15:45:29 -0500

    cleanup

--- debt_belt.ino
+++ debt_belt.ino
@@ -1,9 +1,6 @@
 #include <Stepper.h>
-const int interval = 31500;
+const int interval = 31500;//wait 31.5 seconds to tighten
 const int stepsTightened = 100;  // change this to fit the number of steps per revolution
-#define isTightening false;
-#define startPin
-#define endPin
 
 Stepper myStepper(stepsTightened, 8, 10, 9, 11);
 
@@ -16,30 +13,12 @@ void setup() {
 void loop() {
   //TODO setup full functionality
   // press 'start' button
-  // isTightening = true
-  // run setInitDistance  once
-  // if (isTightening) tighten belt N degrees every N minutes
-  // press 'end' button
-  // if (!isTightening) do nothing
-   // set the speed at 20 rpm:
+  //tighten belt
   myStepper.step(-stepsTightened);
   delay(500);
+  //release belt
   myStepper.step(stepsTightened);
+  //wait interval then repeat
   delay(interval);
 
-}
-
-void startTightening() {
-  //prob just set switch variable here
-  
-}
-
-void endTightening() {
-  
-}
-
-void setInitialDistance() {
-  //TODO
-  //open tightener then tighten to degrees belt is fastened
-  //how to track this?
 }
