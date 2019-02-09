c6080de47083dea545a276e8b96aa85b865983f1
Brent Bailey <bb2906@nyu.edu>
2019-02-09 15:12:19 -0500

    functioning prototype

---
+++ debt_belt.ino
@@ -0,0 +1,44 @@
+#include <Stepper.h>
+const int interval = 10000;
+const int stepsTightened = 100;  // change this to fit the number of steps per revolution
+#define isTightening false;
+#define startPin
+#define endPin
+
+Stepper myStepper(stepsTightened, 8, 10, 9, 11);
+
+void setup() {
+  // put your setup code here, to run once:
+   myStepper.setSpeed(20);
+   Serial.begin(9600);
+}
+
+void loop() {
+  //TODO setup full functionality
+  // press 'start' button
+  // isTightening = true
+  // run setInitDistance  once
+  // if (isTightening) tighten belt N degrees every N minutes
+  // press 'end' button
+  // if (!isTightening) do nothing
+   // set the speed at 20 rpm:
+  Serial.println("clockwise");
+  myStepper.step(-stepsTightened);
+  delay(interval);
+
+}
+
+void startTightening() {
+  //prob just set switch variable here
+  
+}
+
+void endTightening() {
+  
+}
+
+void setInitialDistance() {
+  //TODO
+  //open tightener then tighten to degrees belt is fastened
+  //how to track this?
+}
